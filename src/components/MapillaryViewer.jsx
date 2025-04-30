18c91312ff3424bc13dc3f1c2e0a7443508149cb
saitomasaki21 <100047297+saitomasaki21@users.noreply.github.com>
2025-04-30 17:38:30 -0400

    Update MapillaryViewer.jsx

--- src/components/MapillaryViewer.jsx
+++ src/components/MapillaryViewer.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useRef } from 'react';
 import { Viewer } from 'mapillary-js';
 
@@ -12,33 +13,19 @@ export const MapillaryViewer = ({ accessToken, imageId, viewerRef }) => {
       container: mapillaryContainerRef.current,
       imageId,
       component: {
-        cover: true,     // Esconde elementos de sobreposição
+        cover: false,
         direction: false,
-        sequence: false, // Remove a barra de sequência
+        sequence: false,
         zoom: false,
-        attribution: false // Remove a atribuição
       },
-      renderMode: 'fill'
     });
-
-    const resizeObserver = new ResizeObserver(() => {
-      viewerRef.current.resize();
-    });
-    
-    resizeObserver.observe(mapillaryContainerRef.current);
 
     return () => {
       if (viewerRef.current) {
         viewerRef.current.remove();
       }
-      resizeObserver.disconnect();
     };
   }, [accessToken, imageId, viewerRef]);
 
-  return (
-    <div 
-      ref={mapillaryContainerRef} 
-      className="w-full h-full bg-gray-800" // Fundo neutro
-    />
-  );
+  return <div ref={mapillaryContainerRef} className="w-1/2 h-full" />;
 };
